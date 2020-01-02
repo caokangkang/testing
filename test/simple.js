aa11e78c2d73aa2510b40561e6bbb6e4e16f9101
caokangkang <hkd_ckk@163.com>
2020-01-02 09:32:37 +0800

    去除simple.js里的 无用代码

--- test/simple.js
+++ test/simple.js
@@ -2,12 +2,7 @@
 const {should, expect, assert} = require("chai");
 const {add, mul} = require("../src/math.js");
 
-/* if (add(2, 3) == 5) {
-    console.log('add(2, 3) === 5, ok')
-}else{
-    console.log('add(2, 3) !== 5, error')
-} */
-// assert.equal(mul(2, 3), 6);
+
 should();
 
 add(2, 3).should.equal(5);
